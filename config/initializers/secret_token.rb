ecba3242358d8ad7872795fb2a3c222fe37add1f
Resat Yigit <Resat.Yigit@googlemail.com>
2013-11-05 12:58:56 +0100

    erstes commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uebung4::Application.config.secret_key_base = '3c3346c20e4396ee1c0488e7379acdc3777b1d855791e0f34a7bea7901f780dcfedd6e759a3f11f968927bfa55a5ab789531597cdd603378884f2c4220ba1bcc'
